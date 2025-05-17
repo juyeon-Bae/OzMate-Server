868174cee4b1d563c363ba966dc10359c737cdbf
juyeonBae <w2316@e-mirim.hs.kr>
2025-05-17 16:21:57 +0900

    feat : 북마크 추가 api

---
+++ src/routes/bookMarkRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const BookMark = require('../models/Bookmark')
+const InfoCard = require('../models/CardInfo')
+
+//북마크 추가
+router.post('/', async (req, res) => {
+    const { userId, cardId } = req.body;
+
+    try {
+        const existing = await BookMark.findOne({ userId, cardId })
+
+        if (existing) {
+            return res.status(400).json({ message: '이미 북마크 됨' })
+        }
+
+        const newBookmark = await new BookMark({ userId, cardId }).save();
+        res.status(201).json({ message: '북마크 추가 성공', '북마크 한 정보 카드': newBookmark })
+    }
+    catch (err) {
+        res.status(500).json({ message: '북마크 추가 실패', error: err });
+    }
+})
+
+module.exports = router;
